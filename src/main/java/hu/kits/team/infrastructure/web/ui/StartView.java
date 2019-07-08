a45371c463fd4185732ee65de5fd046be320d809
Kocso Sandor Gabor <kocso.sandor.gabor@gmail.com>
2019-07-08 18:23:50 +0200

    sets session timeout to 12 hours

--- src/main/java/hu/kits/team/infrastructure/web/ui/StartView.java
+++ src/main/java/hu/kits/team/infrastructure/web/ui/StartView.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
 
 import hu.kits.team.infrastructure.web.ui.view.LoginView;
 
@@ -12,6 +13,7 @@ public class StartView extends Div implements BeforeEnterObserver {
 
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
+        VaadinSession.getCurrent().getSession().setMaxInactiveInterval(60 * 60 * 12);
         event.forwardTo(LoginView.class);
     }
 
